d8f03747926eb54d4752d1200e80a1dba19a4882
Hamish Ando <hamish.anderson.21@my.soc.tas.edu.au>
2021-08-17 12:47:31 +1000

    C

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-import pyEX
+import pyEX as p
+import pandas as pd
+import json
 
+APIkey = "Tsk_f518156d6fe3412596ff0e6f3263477a"
+c = p.Client(APIkey)
 
-print("Hello World")
+df = c.incomeStatement("AAPL")[0]
+print(df['ebit'])
