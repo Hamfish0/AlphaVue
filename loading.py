8c35bf8c8e1f26ab2e21a48349d2bf513479f22a
Hamish Ando <hamish.anderson.21@my.soc.tas.edu.au>
2021-08-31 19:16:28 +1000

    buid

--- loading.py
+++ loading.py
@@ -10,7 +10,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Form(object):
+class loadingWindow(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(794, 273)
--- main.py
+++ main.py
@@ -1,8 +1,43 @@
+# =========================================
+# AlphaVue Version 1.0 
+# US stock analysis program
+# -----------------------------------------
+# Hamish Anderson | Computer Science 2021
+# =========================================
+
+# Import packages for application to work.
 import pyEX as p
 import pandas as pd
-from PyQt5 import QtCore, QtGui, QtWidgets
-import json
 import sys
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui as qtg
+
+# Import custom classes from individual application windows.
+from analysiswindow import analysisWindow
+from searchwindow import searchWindow
+from loading import loadingWindow
+
+
+class MainWindow(qtw.QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Your code will go here
+
+        self.loading = loadingWindow()
+        self.search = searchWindow()
+        self.loading.setupUi(self)
+
+
+        # Your code ends here
+        self.show()
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    w = MainWindow()
+    sys.exit(app.exec_())
 
 
 
