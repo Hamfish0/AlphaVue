8aa3f3b1d9c600ab05bd9a79eb7bd1dea403265e
Hamish Ando <hamish.anderson.21@my.soc.tas.edu.au>
2021-08-31 12:29:37 +1000

    build

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pyEX as p
 import pandas as pd
+from PyQt5 import QtCore, QtGui, QtWidgets
 import json
+import sys
+
+
 
 APIkey = "Tsk_f518156d6fe3412596ff0e6f3263477a"
 c = p.Client(APIkey)
 
 df = c.incomeStatement("AAPL")[0]
 print(df['ebit'])
+
