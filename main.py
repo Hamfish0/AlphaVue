bfc415e16810ef61fa9685b469a92c86a2156d3d
Hamish Ando <hamish.anderson.21@my.soc.tas.edu.au>
2021-08-31 23:12:59 +1000

    pu

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+#
 # =========================================
 # AlphaVue Version 1.0 
 # US stock analysis program
 # -----------------------------------------
 # Hamish Anderson | Computer Science 2021
 # =========================================
+#
 
 # Import packages for application to work.
 import pyEX as p
 import pandas as pd
+import time
 import sys
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 from PyQt5 import QtGui as qtg
+from PyQt5.QtGui import QPixmap
 
-# Import custom classes from individual application windows.
+# Import custom classes from individual application widgets.
 from analysiswindow import analysisWindow
 from searchwindow import searchWindow
 from loading import loadingWindow
@@ -21,17 +25,34 @@ from loading import loadingWindow
 
 class MainWindow(qtw.QWidget):
 
+    def on_searchBTNclick(self):
+        print("Button clicked!")
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Your code will go here
 
+        # Initialise application classes to self
         self.loading = loadingWindow()
         self.search = searchWindow()
-        self.loading.setupUi(self)
+        self.analysis = analysisWindow()
 
+        # Create application window objects
+        self.searchQTwindow = qtw.QMainWindow()
+        self.search.setupUi(self.searchQTwindow)
+
+        self.loading.setupUi(self)
+        logo = QPixmap("alphavuelogo.PNG")
+        self.loading.logoLabel.setPixmap(logo)
 
         # Your code ends here
         self.show()
+        self.searchQTwindow.show()
+
+
+
+
+        #self.search.searchButton.clicked.connect(self.on_searchBTNclick)
 
 
 if __name__ == '__main__':
